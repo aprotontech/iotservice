c82afbfd11013aa310f61ea99a72cd064b4b8723
kuper <kuper@aproton.tech>
2022-02-15 17:22:48 +0800

    fix startup script, add deploy document

--- build/deploy/start.sh
+++ build/deploy/start.sh
@@ -4,11 +4,33 @@ set -ex
 
 PROJECT_ROOT=/root/iotservice
 
-service mysql start
+cd $PROJECT_ROOT
 
 if [ ! -f .env ]; then
   cp .env.example .env
   php artisan key:generate
+fi
+
+if [ "$SERVER_MYSQL_HOST" != "localhost" && "$SERVER_MYSQL_HOST" != "127.0.0.1" ]; then
+  # use custome mysql
+  # TODO: change mysql host, port, username, password, database
+  echo "custom mysql-host not support now"
+  exit
+else
+  service mysql start
+fi
+
+if [ ! -f /root/project-inited ]; then
+  sleep 1
+  # create database and add new user
+  mysql -uroot -e "create database aproton"
+  mysql -uroot -e "CREATE USER 'aproton' IDENTIFIED WITH mysql_native_password BY 'LDloAczOvBu7GkeP'"
+  mysql -uroot -e "grant all privileges on aproton.* to 'aproton'@'%'"
+  # init project tables and default admin user
+  php artisan migrate
+  php artisan db:seed ProtonSystemSeeder
+
+  touch /root/project-inited
 fi
 
 php artisan serve --host=0.0.0.0 --port=8080
